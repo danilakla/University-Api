b28b41bb2f0f9033d942c0e2f821c58b64a1c1fe
danila <danila.kla@mail.ru>
2023-04-02 19:32:50 +0300

    Добавьте файлы проекта.

---
+++ University-Api/Program.cs
@@ -0,0 +1,29 @@
+using University_Api.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapControllers();
+
+
+app.Run();
