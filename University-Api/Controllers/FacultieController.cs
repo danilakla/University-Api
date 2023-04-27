d014018b0e994ef73c4ab46bd2d33b54f644bb47
danila <danila.kla@mail.ru>
2023-04-27 23:39:36 +0300

    add method for getting facultie by name of facultie

--- University-Api/Controllers/FacultieController.cs
+++ University-Api/Controllers/FacultieController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityApi.Data;
 using UniversityApi.Services.FacultieService;
 
 namespace UniversityApi.Controllers;
@@ -7,10 +9,12 @@ namespace UniversityApi.Controllers;
 public class FacultieController : ControllerBase
 {
     private readonly IFacultieService _facultieService;
+    private readonly ApplicationDbContext _applicationDbContext;
 
-    public FacultieController(IFacultieService facultieService)
+    public FacultieController(IFacultieService facultieService, ApplicationDbContext applicationDbContext)
     {
         _facultieService = facultieService;
+        _applicationDbContext = applicationDbContext;
     }
     [HttpGet("/get-faculties")]
     public async Task<List<string>> GetFaculties()
@@ -19,6 +23,22 @@ public class FacultieController : ControllerBase
         {
             var faculties = await _facultieService.GetFacultiesAsync();
             return faculties;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+
+    [HttpGet("/get-facultiByName/{name}")]
+    public async Task<IActionResult> GetFaculties(string name)
+    {
+        try
+        {
+            var facultieId = await _applicationDbContext.Faculties.Where(e=>e.Name==name).FirstOrDefaultAsync();
+            return Ok(new { Id= facultieId.FacultieId });
         }
         catch (Exception)
         {
